55b97ef286c0802347d62d94786502de0c7c0b74
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-29 19:42:27 +0200

    Already did this project just commiting

---
+++ script.js
@@ -0,0 +1,78 @@
+let computerPlay=()=>{
+    let ar=["rock","paper","scissors"]
+    let x =Math.floor(Math.random()*3);
+    return ar[x];
+  }
+let playerSelectionn=()=>{
+  let playerSelection=prompt("choose rock paper scissors");
+    while(true){
+      playerSelection=playerSelection.toLowerCase();
+// console.log(playerSelection);
+      if(playerSelection==="rock"||playerSelection==="paper"|| playerSelection==="scissors"){
+        break;
+      }else{
+        alert("please enter a either rock, paper or scissors");
+        playerSelection=prompt("choose rock paper scissors");
+      }
+  }
+    return playerSelection;
+}
+
+let play=(computerSelection,playerSelection)=>{
+  console.log("Your selection is "+playerSelection);
+  console.log("Computer selection is "+computerSelection);
+  let message="";
+    switch(playerSelection){
+      case "rock":
+        if (computerSelection==="paper"){
+        message="You lose! "+computerSelection+" beats "+playerSelection;
+        }else if(computerSelection==="scissors"){
+          message="You win! "+playerSelection+" beats "+computerSelection;
+        }else{
+          message="Draw! you both selected "+computerSelection;
+        }
+        break;
+      case "paper":
+        if (computerSelection==="scissors"){
+        message="You lose! "+computerSelection+" beats "+playerSelection;
+        }else if(computerSelection==="rock"){
+          message="You win! "+playerSelection+" beats "+computerSelection;
+        }else{
+          message="Draw! you both selected "+computerSelection;
+        }
+        break;
+      case "scissors":
+        if (computerSelection==="rock"){
+        message="You lose! "+computerSelection+" beats "+playerSelection;
+        }else if(computerSelection==="paper"){
+          message="You win! "+playerSelection+" beats "+computerSelection;
+        }else{
+          message="Draw! you both selected "+computerSelection;
+        }
+        break;
+    }
+    return message;
+  }
+  // console.log(play(computerPlay(),playerSelection()));
+
+let game=()=>{
+  let playerScore=0;
+  let computerScore=0;
+  for(let i=0;i<5;i++){
+    let result=play(computerPlay(),playerSelectionn())
+    console.log(result);
+    if(result.includes("win")){
+      playerScore++;
+    }else if(result.includes("lose")){
+      computerScore++;
+    }
+  }
+  if(playerScore>computerScore){
+    console.log("Player wins");
+  }else if(playerScore<computerScore){
+    console.log("Computer wins");
+  }else{
+    console.log("Draw");
+  }
+}
+game();

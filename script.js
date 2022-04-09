8225ea1232ebb7ac140ab2669866f36f4ca67ce9
Salah A.Fattah <salahfattah1@outlook.com>
2022-04-09 19:59:32 +0200

    Add styling and reset button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bod=document.querySelector("body");
+const butotns=document.querySelector(".butotns");
 
 let result=[0,0];
 const buttons =document.querySelectorAll("button");
@@ -6,18 +7,64 @@ buttons.forEach(button=>{
   button.addEventListener("click",e=>{
     
     let selection= e.target.textContent.toLowerCase();
-    // console.log(selection);
     let score=game(computerPlay(),selection);
-    
+    let draw=document.querySelector("#who-won");
+    draw.style.display="none";
+    bod.insertBefore(draw,butotns);
+    if(score[0]===0&&score[1]===0){
+      draw.textContent="Draw!";
+      draw.style.display="block";
+    } else if(score[0]===1){
+      draw.textContent="OOOOH YEAAH! you won this round";
+      draw.style.display="block";
+    }else{
+      draw.textContent="OOOOH NO! you lost this round";
+      draw.style.display="block";
+    }
     result[0]+=score[0];
     result[1]+=score[1];
+    
+    let comScore=document.querySelector("#computer-score");
+    comScore.textContent=`Computer: ${result[1]} points`;
+    let playScore=document.querySelector("#player-score");
+    playScore.textContent=`Player ${result[0]} points`;
+    let finalScore=document.querySelector("#final-score");
     if(result[0]===5){
-      console.log("Game Over Player Wins!");
+      butotns.style.display="none";
+      finalScore.textContent="Game Over Player Wins!";
+      let butt=document.createElement("button");
+      bod.appendChild(butt);
+      butt.textContent="Play Again";
+      butt.addEventListener("click",e=>{
+        butotns.style.display="inline-block";
+        result[0]=0;
+        result[1]=0;
+        butt.remove();
+        finalScore.textContent="";
+        draw.textContent="";
+        comScore.textContent=`Computer: ${result[1]} points`;
+        playScore.textContent=`Player ${result[0]} points`;
+      })
+      
     }else if (result[1]===5){
-      console.log("Game Over Computer Wins!");
+      butotns.style.display="none";
+      finalScore.textContent="Game Over Computer Wins!";
+      let butt=document.createElement("button");
+      bod.appendChild(butt);
+      butt.textContent="Play Again";
+      butt.addEventListener("click",e=>{
+        butotns.style.display="inline-block";
+        result[0]=0;
+        result[1]=0;
+        butt.remove();
+        finalScore.textContent="";
+        draw.textContent="";
+        comScore.textContent=`Computer: ${result[1]} points`;
+        playScore.textContent=`Player ${result[0]} points`;
+      })
     }
-    console.log(result);
-    // play(computerPlay(),selection);
+    
+
     
   })
 })
@@ -28,95 +75,39 @@ let computerPlay=()=>{
     let x =Math.floor(Math.random()*3);
     return ar[x];
   }
-// let playerSelectionn=()=>{
-//   let playerSelection=prompt("choose rock paper scissors");
-//     while(true){
-//       playerSelection=playerSelection.toLowerCase();
-// // console.log(playerSelection);
-//       if(playerSelection==="rock"||playerSelection==="paper"|| playerSelection==="scissors"){
-//         break;
-//       }else{
-//         alert("please enter a either rock, paper or scissors");
-//         playerSelection=prompt("choose rock paper scissors");
-//       }
-//   }
-//     return playerSelection;
-// }
 
 let play=(computerSelection,playerSelection)=>{
-  let playerSelect=document.createElement("div");
-  playerSelect.textContent="Your selection is "+playerSelection;
-  bod.appendChild(playerSelect);
-  let computerSelect=document.createElement("div");
-  computerSelect.textContent="Computer selection is "+computerSelection;
-  bod.appendChild(computerSelect);
-  let message="";
+  let points=[0,0];
     switch(playerSelection){
       case "rock":
         if (computerSelection==="paper"){
-        message="You lose! "+computerSelection+" beats "+playerSelection;
+        points[1]=1;
         }else if(computerSelection==="scissors"){
-          message="You win! "+playerSelection+" beats "+computerSelection;
-        }else{
-          message="Draw! you both selected "+computerSelection;
+          points[0]=1;
         }
         break;
       case "paper":
         if (computerSelection==="scissors"){
-        message="You lose! "+computerSelection+" beats "+playerSelection;
+        points[1]=1;
         }else if(computerSelection==="rock"){
-          message="You win! "+playerSelection+" beats "+computerSelection;
-        }else{
-          message="Draw! you both selected "+computerSelection;
+          points[0]=1;
         }
         break;
       case "scissors":
         if (computerSelection==="rock"){
-        message="You lose! "+computerSelection+" beats "+playerSelection;
+        points[1]=1;
         }else if(computerSelection==="paper"){
-          message="You win! "+playerSelection+" beats "+computerSelection;
-        }else{
-          message="Draw! you both selected "+computerSelection;
+          points[0]=1;
         }
         break;
     }
-    return message;
+    return points;
   }
-  // console.log(play(computerPlay(),playerSelection()));
 
 let game=(complay,player)=>{
   let scores=[];
-  let playerScore=0;
-  let computerScore=0;
-  let finalResult="";
-  // for(let i=0;i<5;i++){
-    let result=play(complay,player);
-    // let resultNode=document.createElement("div");
-    // resultNode.textContent=result;
-    // bod.appendChild(resultNode);
-    // console.log(result);
-    if(result.includes("win")){
-      playerScore++;
-      
-    }else if(result.includes("lose")){
-      computerScore++;
-      
-    }
-    scores.push(playerScore);
-    scores.push(computerScore);
-    // console.log("player score",playerScore);
-    // console.log("computer score",computerScore);
-  // }
-  // if(playerScore>computerScore){
-  //   finalResult="Player wins";
-  // }else if(playerScore<computerScore){
-  //   finalResult="Computer wins";
-  // }else{
-  //   finalResult="Draw";
-  // }
-  // let finalNode=document.createElement("div");
-  // finalNode.textContent=finalResult;
-  // bod.appendChild(finalNode);
+  let result=play(complay,player);
+  scores.push(result[0]);
+  scores.push(result[1]);
   return scores;
 }
-// game();
